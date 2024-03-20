06140ff4df6bdb5954259e63c41a0729e3b86c7c
cfontelar <fontelarcj@gmail.com>
2024-03-20 15:26:46 -0500

    Modified relationship of image to the product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,20 @@
 class Product < ApplicationRecord
-    has_many_attached :images
+    belongs_to :category
+    has_one_attached :image
+  
+    # Attributes that are allowed to be searched by Ransack
+    def self.ransackable_attributes(auth_object = nil)
+      %w[name description price quantity created_at updated_at]
+    end
+  
+    # Specify which associations can be used for searching
+    def self.ransackable_associations(auth_object = nil)
+      %w[category]
+    end
+  
+    # Attributes that will not be searchable
+    def self.unransackable_attributes(auth_object = nil)
+      %w[id]
+    end
   end
+  
