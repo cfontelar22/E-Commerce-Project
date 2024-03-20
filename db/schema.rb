5c0b11972f4e676b664c495694d98a270ea56076
cfontelar <fontelarcj@gmail.com>
2024-03-20 15:27:27 -0500

    Modifications for product errors in admin dashboard!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_20_171808) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_20_182648) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -75,6 +75,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_171808) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
