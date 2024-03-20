f9f06a3600e6f14a5e92bac4e37b94a23c3e5aec
cfontelar <fontelarcj@gmail.com>
2024-03-20 15:24:42 -0500

    Redirected to session path for new admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def authenticate_admin!
+        redirect_to new_admin_session_path unless current_admin && current_admin.is_a?(Admin)
+      end      
 end
