e6c8c5f888cb897874ed24c4cf5fb7d7ac629ad9
cfontelar <fontelarcj@gmail.com>
2024-03-20 21:12:45 -0500

    Updated Home page as the landing page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,19 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :admins, ActiveAdmin::Devise.config
 
-  # Root path for landing page
-  root to: 'admin/dashboard#index'
-
   # Static Pages routes
   get '/about', to: 'pages#about', as: 'about'
   get '/contact', to: 'pages#contact', as: 'contact'
 
-   # Route for the products listing
-   get 'products', to: 'products#index', as: 'products'
-  
-   # Route for filtering by category
-   get 'categories/:category', to: 'products#index', as: 'category_products'
-   
-   # Route for product details
-   get 'products/:id', to: 'products#show', as: 'product'
-   
+  # Root path for landing page
+  root to: 'pages#home'
+
+  # Products routes
+  resources :products, only: [:index, :show]
+
+  # Categories routes
+  resources :categories, only: [:index, :show] do
+    # Nested route for products under a category
+    resources :products, only: :index, as: :category_products
+  end
 end
