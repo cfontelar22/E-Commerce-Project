12a8de880efa4b990cc89f3cf39a9072255dfa7e
cfontelar <fontelarcj@gmail.com>
2024-03-21 22:15:56 -0500

    Added attributes for an individual product display.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Product.destroy_all
 Category.destroy_all
 
 # Create categories
-unique_categories = ['Single Origin', 'Blended', 'Espresso', 'Light Roast', 'Dark Roast']
+unique_categories = ['Single Origin', 'Blended', 'Espresso', 'Light Roast','Medium Roast' 'Dark Roast']
 unique_categories.each do |category_name|
   Category.find_or_create_by!(name: category_name)
 end
@@ -20,22 +20,27 @@ puts "Created #{Category.count} categories."
 # Fetch the data from the API
 response = HTTParty.get('https://fake-coffee-api.vercel.app/api/')
 if response.success?
-  products_data = response.parsed_response * 5 # Multiply the array by 5 to get 100 products
+  products_data = response.parsed_response * 5 
 
   products_data.each_with_index do |product_data, index|
     # Find the specific category or use 'General'
     category_name = product_data['flavor_profile'].first
     category = Category.find_by(name: category_name) || general_category
-  
-    # Create the product without appending the index to the name
+
+    # Create the product with the information from the API
     product = Product.create(
-      name: product_data['name'], # Use the original name without a number
+      name: product_data['name'],
       description: product_data['description'],
-      price: product_data['price'].to_f + Faker::Commerce.price(range: 0..0.99), 
+      price: product_data['price'].to_f + Faker::Commerce.price(range: 0..0.99),
       quantity: product_data['inventoryCount'],
-      category: category
+      category: category,
+      region: product_data['region'], 
+      weight: product_data['weight'],
+      roast_level: product_data['roast_level'],
+      flavor_profile: product_data['flavor_profile'].join(', '),
+      grind_option: product_data['grind_option'].join(', ') 
     )
-  
+
     # Attach the image if the product was saved and an image URL is available
     if product.persisted? && product_data['image_url'].present?
       begin
