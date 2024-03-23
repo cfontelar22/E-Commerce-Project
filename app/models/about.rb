e972a50ae8c0a6c4a29f95588e9a1e86e565025b
cfontelar <fontelarcj@gmail.com>
2024-03-23 13:57:06 -0500

    Implement dynamic content for contact and about pages

--- app/models/about.rb
+++ app/models/about.rb
@@ -1,2 +1,11 @@
 class About < ApplicationRecord
+
+    # Whitelist attributes for Ransack search
+  def self.ransackable_attributes(auth_object = nil)
+    %w[content created_at updated_at]
+  end
+
+  def self.unransackable_attributes(auth_object = nil)
+    ['id', 'some_other_attribute']
+  end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,11 @@
 class Contact < ApplicationRecord
+    # Only allow these attributes to be searchable by Ransack
+  def self.ransackable_attributes(auth_object = nil)
+    %w[content created_at updated_at]
+  end
+
+  # Exclude all other attributes from being searchable by Ransack
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
