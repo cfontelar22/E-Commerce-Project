7a3542dcd9682d5aa302248bafe1e560cb40ddde
cfontelar <fontelarcj@gmail.com>
2024-04-12 00:22:58 -0500

    Fix Order Summary errors

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,20 +11,16 @@ class OrdersController < ApplicationController
     @order = current_customer.orders.build(order_params)
     build_order_items(@cart)
 
-
     if @order.save
-      # Clear the cart session
-      session[:cart_items] = nil
-      # Redirect to the show order path with the notice
+      session[:cart_items] = nil  # Clear the cart session
       redirect_to order_path(@order), notice: 'Order was successfully placed.'
     else
-      # If there are errors, log them and redirect back
       Rails.logger.info "Order save failed: #{@order.errors.full_messages.join(", ")}"
-      flash[:alert] = 'There was a problem placing your order: ' + @order.errors.full_messages.to_sentence
+      flash[:alert] = 'There was a problem placing your order.'
       redirect_back(fallback_location: root_path)
     end
   end
-  
+
   private
 
   def set_cart_data
@@ -32,10 +28,15 @@ class OrdersController < ApplicationController
   end
 
   def build_order_items(cart)
-    
     cart.items.each do |item_data|
-      product = Product.find(item_data['product_id'])
-      unless product.nil?
+      product = Product.find_by(id: item_data['product_id'])
+      next if product.nil?
+
+      # Check if the item has already been added
+      existing_item = @order.order_items.find { |oi| oi.product_id == product.id }
+      if existing_item
+        existing_item.quantity += item_data['quantity']
+      else
         @order.order_items.build(
           product: product,
           quantity: item_data['quantity'],
@@ -44,13 +45,12 @@ class OrdersController < ApplicationController
       end
     end
   end
-  
-  
+
   def order_params
-  params.require(:order).permit(:subtotal, :gst, :pst, :hst, :total,
-                                customer_attributes: [:address, :city, :province, :postal_code, :phone_number],
-                                order_items_attributes: [:product_id, :quantity, :price])
-end
+    params.require(:order).permit(:subtotal, :gst, :pst, :hst, :total,
+                                  customer_attributes: [:address, :city, :province, :postal_code, :phone_number],
+                                  order_items_attributes: [:product_id, :quantity, :price])
+  end
   
 
   def address_provided?
